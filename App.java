c99363a0be44a8e4802f3194f804b8153e9a7cb7
Madhav-Koli <kolimadhav2012@gmail.com>
2023-12-29 12:45:01 +0530

    one for loop

--- App.java
+++ App.java
@@ -1,10 +1,19 @@
- 
+
 package com.flipkart;
 
 
  public class App{
+	 int n = 10;
 	 public static void main(String []args){
 		 System.out.println("Hello world");
 		 System.out.println("Hello world");
+             
+		 for(int i=0; i<=n; i++){
+                   
+		    if(n % i == 0){
+	              Sytem.out.println(i);
+                   } 		      
+
 	 }
+	  
  }
